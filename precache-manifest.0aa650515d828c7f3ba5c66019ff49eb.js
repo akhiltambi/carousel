9426d849bea569e4a6f08ad9a3f4ec98b8677fdc
Akhil Tambi <akhiltambi@gmail.com>
2020-09-10 20:22:36 +0530

    Updates

---
+++ precache-manifest.0aa650515d828c7f3ba5c66019ff49eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c66bc61b54bc91acccb145223c27e7b",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "f6a5399e0d5ce8a65d60",
+    "url": "/carousel/static/css/2.702c6a11.chunk.css"
+  },
+  {
+    "revision": "de7394bfe4a291cad2fb",
+    "url": "/carousel/static/css/main.a0cd1e7f.chunk.css"
+  },
+  {
+    "revision": "f6a5399e0d5ce8a65d60",
+    "url": "/carousel/static/js/2.d840fcba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/carousel/static/js/2.d840fcba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de7394bfe4a291cad2fb",
+    "url": "/carousel/static/js/main.d38d84fd.chunk.js"
+  },
+  {
+    "revision": "010921712ac8071e07e4",
+    "url": "/carousel/static/js/runtime-main.596077f8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/carousel/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/carousel/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/carousel/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/carousel/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/carousel/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
